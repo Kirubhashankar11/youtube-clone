340c9b2b197cce17b36a3462d1446ef56f292a42
Kirubhashankar11 <112264549+Kirubhashankar11@users.noreply.github.com>
2024-03-13 05:43:43 +0000

    -1:19

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var menuIcon = document.querySelector(".menu-icon")
-var menuIcon = document.querySelector(".menu-icon")
-var menuIcon = document.querySelector(".menu-icon")
+var sidebar = document.querySelector(".sidebar")
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar")
+}
